241345dada73e5fed656abe6dd7b60b9bf05fd09
a0903127277 <a0903127277@gmail.com>
2023-03-14 23:17:41 +0800

    second test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShareModule } from './share/share.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
   ],
   imports: [
     ShareModule,
