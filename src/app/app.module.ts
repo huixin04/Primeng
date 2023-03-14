146758ef8bcd3cdfb0733e4aa677652c2e3a5203
huixin04 <a0903127277@gmail.com>
2023-03-14 14:01:50 +0800

    First tst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ShareModule } from './share/share.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ShareModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
